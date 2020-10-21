35c32ac2643cf61e4b0db578b99616ccf1d61714
Arifannyrs <arifannyrs@gmail.com>
2020-10-21 10:51:28 +0700

    UTS

---
+++ Assets/Scripts/HalamanUtama.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class HalamanUtama : MonoBehaviour
+{
+    public void PindahHalaman(string halaman){
+        SceneManager.LoadScene(halaman);
+    }
+    public void KeluarAplikasi(){
+        Application.Quit();
+    }
+}
---
+++ Assets/TangkapObject.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TangkapObject : MonoBehaviour
+{
+    public GameObject bola;
+    public GameObject tangan;
+    public Text tulisan;
+
+    bool tertangkap = false;
+    Vector3 posisiBola;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posisiBola = bola.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetButtonDown("Fire1"))
+        {
+            if(!tertangkap){
+            bola.transform.SetParent(tangan.transform);
+            bola.transform.localPosition = new Vector3(0f, -.672f,0f);
+            bola.GetComponent<Renderer>().material.color = Color.blue;
+            tulisan.text = "Bola ditangkap";
+            tertangkap = true;
+            
+            } else if (tertangkap){
+                bola.transform.SetParent(null);
+                bola.transform.localPosition = posisiBola;
+                bola.GetComponent<Renderer>().material.color = Color.red;
+                tulisan.text = "Bola dilepas";
+                tertangkap = false;
+            }
+        }
+    }
+}
